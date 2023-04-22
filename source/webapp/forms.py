94bc81647167589c2792e4cd86cb02d5a59e71fa
Alibek Alikhan <alialikhan0520@gmail.com>
2023-04-22 19:03:15 +0600

    Сделал формы для Crud #2

---
+++ source/webapp/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+
+from webapp.models import Gallery
+
+
+class GalleryForm(forms.ModelForm):
+    class Meta:
+        model = Gallery
+        fields = ("photo", "signature")
+        labels = {
+            'photo': 'Фото',
+            'signature': 'Подпись',
+        }
